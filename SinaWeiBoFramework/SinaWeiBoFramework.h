8cc2f422cc4188ec1fa63e5a9459051cffe5c2a2
quanchengwu <quancheng.wu@gmail.com>
2013-07-21 16:49:55 +0800

    import

---
+++ SinaWeiBoFramework/SinaWeiBoFramework.h
@@ -0,0 +1,13 @@
+//
+//  SinaWeiBoFramework.h
+//  SinaWeiBoFramework
+//
+//  Created by Wuquancheng on 12-8-16.
+//  Copyright (c) 2012年 Wuquancheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SinaWeiBoFramework : NSObject
+
+@end
---
+++ SinaWeiBoFramework/SinaWeiBoFramework.m
@@ -0,0 +1,13 @@
+//
+//  SinaWeiBoFramework.m
+//  SinaWeiBoFramework
+//
+//  Created by Wuquancheng on 12-8-16.
+//  Copyright (c) 2012年 Wuquancheng. All rights reserved.
+//
+
+#import "SinaWeiBoFramework.h"
+
+@implementation SinaWeiBoFramework
+
+@end
